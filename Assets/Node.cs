fb0e33f67a47501053717543c174b034b634d846
YuChuan Lin <43222074+Eggric0802@users.noreply.github.com>
2020-05-02 21:33:34 -0500

    Shooting Effect
    Particle System
    Scprits
    Bullets

---
+++ Assets/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = hoverColor;
+    }
+
+    
+}
